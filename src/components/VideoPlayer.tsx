e267ed242b79a47d3fe8c6d2db12bcc13720305f
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-23 19:36:01 +0400

    test: 12 on preload.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -14,21 +14,16 @@ export const VideoPlayer = () => {
         url="https://media-house.s3.amazonaws.com/sizmar-ai-generated-videos/acac352c-32e9-4d15-b3c0-087c6a72a2f8-c7d4afb3-37d7-4e19-afc1-985c9631c4db.mp4"
       />
       <h1>video player</h1>
-      <iframe
-        width="100%"
-        height="100%"
+      <video
         src="https://media-house.s3.amazonaws.com/sizmar-ai-generated-videos/acac352c-32e9-4d15-b3c0-087c6a72a2f8-c7d4afb3-37d7-4e19-afc1-985c9631c4db.mp4"
-      ></iframe>
-      {/* <video
         controls
         id="video"
-        src="./"
         width="100%"
         height="100%"
-        preload="auto"
+        preload="none"
         autoPlay={false}
         muted={true}
-      ></video> */}
+      ></video>
     </div>
   );
 };
