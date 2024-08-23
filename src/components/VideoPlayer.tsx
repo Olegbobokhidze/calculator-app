6208c0e20e06043af1c161dee4c3d780acda2626
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-23 18:51:25 +0400

    test: fourth.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -12,7 +12,7 @@ export const VideoPlayer = () => {
         url="https://media-house.s3.amazonaws.com/sizmar-ai-generated-videos/acac352c-32e9-4d15-b3c0-087c6a72a2f8-c7d4afb3-37d7-4e19-afc1-985c9631c4db.mp4"
       />
       <h1>video player</h1>
-      <video controls width="100%" height="100%" playsInline preload="metadata">
+      <video controls width="100%" height="100%" playsInline preload="auto">
         <source src="https://media-house.s3.amazonaws.com/sizmar-ai-generated-videos/acac352c-32e9-4d15-b3c0-087c6a72a2f8-c7d4afb3-37d7-4e19-afc1-985c9631c4db.mp4" />
       </video>
     </div>
