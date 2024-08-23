4283a6e2c51b7f3b5ddb41be2e5b4a0579d959e3
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-23 18:43:38 +0400

    test: video.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import Keypad from "./components/keypad/Keypad";
 import { GlobalStyle, ThemeOne, ThemeThree, ThemeTwo } from "./Themes";
+import { VideoPlayer } from "./components/VideoPlayer";
 
 function App() {
   const [theme, setTheme] = useState(ThemeOne);
@@ -9,7 +10,8 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Keypad theme={theme} setTheme={setTheme} />
+      {/* <Keypad theme={theme} setTheme={setTheme} /> */}
+      <VideoPlayer />
     </ThemeProvider>
   );
 }
---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,24 @@
+import ReactPlayer from "react-player";
+
+export const VideoPlayer = () => {
+  return (
+    <div style={{ paddingLeft: "20px", paddingRight: "20px" }}>
+      <h1>React player</h1>
+      <ReactPlayer
+        playsinline
+        width="100%"
+        height="100%"
+        controls
+        url="https://media-house.s3.amazonaws.com/sizmar-ai-generated-videos/acac352c-32e9-4d15-b3c0-087c6a72a2f8-c7d4afb3-37d7-4e19-afc1-985c9631c4db.mp4"
+      />
+      <h1>video player</h1>
+      <video
+        controls
+        width="100%"
+        height="100%"
+        playsInline
+        src="https://media-house.s3.amazonaws.com/sizmar-ai-generated-videos/acac352c-32e9-4d15-b3c0-087c6a72a2f8-c7d4afb3-37d7-4e19-afc1-985c9631c4db.mp4"
+      />
+    </div>
+  );
+};
