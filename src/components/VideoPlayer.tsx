8b2cd9e241d93cbedcdff7d7d161c9d1c4e41b5b
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-23 19:43:09 +0400

    fix: ignore silent switch.

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -11,6 +11,7 @@ export const VideoPlayer = () => {
         controls
         volume={0}
         muted={true}
+        ignoreSilentSwitch="ignore"
         url="https://media-house.s3.amazonaws.com/sizmar-ai-generated-videos/acac352c-32e9-4d15-b3c0-087c6a72a2f8-c7d4afb3-37d7-4e19-afc1-985c9631c4db.mp4"
       />
       <h1>video player</h1>
@@ -23,7 +24,7 @@ export const VideoPlayer = () => {
         preload="none"
         autoPlay={false}
         muted={true}
-      ></video>
+      />
     </div>
   );
 };
